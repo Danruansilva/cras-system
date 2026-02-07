60fccdd303a0c3313b4cb9c5a803ef4c7f5ff62b
Danruan da Cruz Silva <danruansilva360@gmail.com>
2026-02-07 16:18:42 -0300

    corrige rota excluir beneficiario

--- config/urls.py
+++ config/urls.py
@@ -1,17 +1,10 @@
-from django.urls import path
-from core import views
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='login'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('cadastro/', views.cadastro_beneficiario, name='cadastro'),
-    path('conceder_cesta/<int:beneficiario_id>/', views.conceder_cesta, name='conceder_cesta'),
-    path('logout/', views.logout_view, name='logout'),
+    # Admin do Django
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')),  # ← só incluir os apps AQUI
-    
+
+    # Todas as URLs do app 'core'
+    path('', include('core.urls')),  # todas as rotas do core serão gerenciadas aqui
 ]
