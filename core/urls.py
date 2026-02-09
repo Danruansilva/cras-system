710d5260989790390b11175aeee816fa907b6d74
Danruan da Cruz Silva <danruansilva360@gmail.com>
2026-02-09 10:52:57 -0300

    Corrige referência da view home no core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -4,12 +4,12 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('login/', views.home, name='home'),
+    path('login/', views.home, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('cadastro/', views.cadastro_beneficiario, name='cadastro'),
     path('conceder-cesta/<int:beneficiario_id>/', views.conceder_cesta, name='conceder_cesta'),
     path('beneficiario/<int:beneficiario_id>/', views.detalhe_beneficiario, name='detalhe_beneficiario'),
     path('logout/', views.logout_view, name='logout'),
-,
+    path('', views.home, name='home'),
     path('excluir/<int:id>/', views.excluir_beneficiario, name='excluir_beneficiario'),
 ]
