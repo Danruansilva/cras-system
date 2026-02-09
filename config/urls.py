3631bddbb3daaf9b4f505ae4a0f645202a749ed8
Danruan da Cruz Silva <danruansilva360@gmail.com>
2026-02-09 11:24:10 -0300

    Corrige namespace core nas URLs

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from core import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +12,6 @@ urlpatterns = [
 
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+     path('', include(('core.urls', 'core'), namespace='core')),
 ]
