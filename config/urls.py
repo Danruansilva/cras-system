08287d56c44b0048276ab1f6655b99e36d8ecb27
Danruan da Cruz Silva <danruansilva360@gmail.com>
2026-02-09 11:18:08 -0300

    Adiciona rota de logout e corrige erro NoReverseMatch

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),  # 👈 manda para o app core
+
+    path('', views.home, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
