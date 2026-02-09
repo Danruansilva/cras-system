7b433dfe946ab5efc4871f42f85498c77edd80a8
Danruan da Cruz Silva <danruansilva360@gmail.com>
2026-02-09 09:50:28 -0300

    Fix criar_superuser not defined

--- core/apps.py
+++ core/apps.py
@@ -1,9 +1,10 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
+from .signals import criar_superuser
 
 class CoreConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "core"
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core'
 
     def ready(self):
         post_migrate.connect(criar_superuser, sender=self)
