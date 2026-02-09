9bf728193a6c6cff440e354c636e97ff4e0543e1
Danruan da Cruz Silva <danruansilva360@gmail.com>
2026-02-09 09:37:49 -0300

    Remove criação automática de superuser

--- core/apps.py
+++ core/apps.py
@@ -1,12 +1,9 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
 
     def ready(self):
-        from django.db.models.signals import post_migrate
         post_migrate.connect(criar_superuser, sender=self)
-
-
