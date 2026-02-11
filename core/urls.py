b109c5d8bef019e6e421fae49fccb65ecf2c12a5
Danruan da Cruz Silva <danruansilva360@gmail.com>
2026-02-11 17:10:53 -0300

    teste

--- core/urls.py
+++ core/urls.py
@@ -4,10 +4,13 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Página inicial = login
+ path('', views.home, name='home'),
     path('login/', views.home, name='login'),
-    path('dashboard/', views.dashboard, name='dashboard'),
     path('logout/', views.logout_view, name='logout'),
+
+    path('dashboard/', views.dashboard, name='dashboard'),
+
+   
     path('cadastro/', views.cadastro_beneficiario, name='cadastro'),
     path('conceder-cesta/<int:beneficiario_id>/', views.conceder_cesta, name='conceder_cesta'),
     path('excluir/<int:beneficiario_id>/', views.excluir_beneficiario, name='excluir'),
