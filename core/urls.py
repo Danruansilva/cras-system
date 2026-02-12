ec6becf8914c01827204d4a37f96ac4671a41fb4
Danruan da Cruz Silva <danruansilva360@gmail.com>
2026-02-12 15:37:09 -0300

    teste

--- core/urls.py
+++ core/urls.py
@@ -1,18 +1,32 @@
 from django.urls import path
 from . import views
 
-app_name = 'core'
+app_name = "core"
 
 urlpatterns = [
- path('', views.home, name='home'),
+    path('', views.home, name='home'),
     path('login/', views.home, name='login'),
     path('logout/', views.logout_view, name='logout'),
 
     path('dashboard/', views.dashboard, name='dashboard'),
 
-   
     path('cadastro/', views.cadastro_beneficiario, name='cadastro'),
-    path('conceder-cesta/<int:beneficiario_id>/', views.conceder_cesta, name='conceder_cesta'),
-    path('excluir/<int:beneficiario_id>/', views.excluir_beneficiario, name='excluir'),
-    path('detalhe/<int:beneficiario_id>/', views.detalhe_beneficiario, name='detalhe'),
+
+    path(
+        'beneficiario/<int:beneficiario_id>/',
+        views.detalhe_beneficiario,
+        name='detalhe_beneficiario'
+    ),
+
+    path(
+        'conceder-cesta/<int:beneficiario_id>/',
+        views.conceder_cesta,
+        name='conceder_cesta'
+    ),
+
+    path(
+        'excluir/<int:beneficiario_id>/',
+        views.excluir_beneficiario,
+        name='excluir_beneficiario'
+    ),
 ]
